75dac78dd9ff0d15ec02e7f05fa573271a252751
Alexandra <alexandra.vladu21@icloud.com>
2022-12-20 15:29:41 +0000

    issues

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {FC, ChangeEvent, useState} from 'react';
+import {BrowserRouter as Router, Routes, Route, Navigate} from 'react-router-dom'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Todo from './components/Todo';
+import NavBar from "./components/nav/NavBar";
+import Home from "./pages/Home";
+import {ITask} from './Interfaces'
+
+
+
+const App: FC = () => {
+
+  return(
+    <>
+        <Router>
+      <Routes>
+        <Route path="/" element={<><NavBar /><Home /></>}></Route>
+        {/* <Route path="/facts" element={<><NavBar/><Facts /> </>}></Route> */}
+        {/* <Route path="/support" element={<><NavBar/><Support /> </>}></Route> */}
+      </Routes>
+    </Router>
+
+    <Todo />
+    </>
+  )
 }
 
 export default App;
