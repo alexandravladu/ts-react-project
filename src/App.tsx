3e78b477fd4fba8e720ccbbb2eaa6ed25de68c4d
anji-braimoh <anjolabraimoh@outlook.com>
2022-12-22 12:52:23 +0000

    first commit on anji2

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { FC } from 'react';
 import './App.css';
+import Sidebar from './components/sideStyling';
+import { BrowserRouter as Router, Route, Routes  } from 'react-router-dom';
+import {  ToDo } from './pages/toDo';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App: FC = () => {
+    return (
+        <Router>
+            <Sidebar />
+            <Routes>
+                <Route path="/overview" element={<ToDo/>} ></Route>
+                
+            </Routes>
+        </Router>
+    );
+};
 
 export default App;
---
+++ src/pages/toDo.tsx
@@ -0,0 +1,6 @@
+import { FC } from 'react';
+
+export const ToDo: FC = () => {
+    return <div className = "ToDo" >To Do </div>;
+};
+
